0aa4da2121274e46bd836ba70dec5a49819343ce
Alex Smith <alexsmith716@outlook.com>
2017-06-26 13:02:10 -0400

    Modified several files. More changes following todo from last push June 24 and first push on June 26. renderableErrorObject is now handling error object configuration for client render. Applied callbacks to all server controller functions and broke out res locals from res method into its own object. Todo next is is to continue applying error handling updates to all modules.

--- shared/renderableErrorObject.js
+++ shared/renderableErrorObject.js
@@ -1,8 +1,9 @@
+
 module.exports = function (err) {
 
-  var errTitle = 'Application Error Notice'
+  var errTitle = 'An Application Error Has Occurred'
 
-  var errAlert = '<p>A error recently occurred with the application.</p><p>If this problem continues, please contact our Help Desk at 555-555-1234 or email Customer Service at customer.care@ThisGreatApp.com.</p><p>Visit our&nbsp;<a class="highlight" href="/customerservice">Customer Service</a>&nbsp;webpage for a full listing of helpful information.</p><p>We appreciate your patience!</p>'
+  var errAlert = '<p>A error recently occurred with the application. Please try your request again.</p><p>If this problem continues, please contact our Help Desk at 555-555-1234.</p><p>You may also email Customer Service at customer.care@ThisGreatApp.com.</p><p>Visit our&nbsp;<a class="highlight" href="/customerservice">Customer Service</a>&nbsp;webpage for a full listing of helpful information.</p><p>We appreciate your patience!</p>'
 
   var errMessage = '<pre><p>Name:&nbsp;'+err.name+'</p><p>Message:&nbsp;'+err.message+'</p><p>Status:&nbsp;'+err.status+'</p><p>Code:&nbsp;'+err.code+'</p><p>Referer:&nbsp;'+err.referer+'</p><p>Stack:&nbsp;'+err.stack+'</p></pre>'
 
--- theServer/controller/serverMainCtrls.js
+++ theServer/controller/serverMainCtrls.js
@@ -42,12 +42,22 @@ var handleError = function (req, res, statusCode) {
 
 module.exports.getLogout = function (req, res, next) {
   req.logout()
-
   req.session.destroy(function (err) {
+
     if (err) {
       return next(err)
+
     } else {
-      res.redirect('/')
+
+      res.redirect('/', function (err, html) {
+
+        if (err) {
+          return next(err)
+
+        } else {
+          res.send(html)
+        }
+      })
 
     }
   })
@@ -57,59 +67,24 @@ module.exports.getLogout = function (req, res, next) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 module.exports.getIndex = function (req, res, next) {
-  var htitle = 'Election App 2016!'
-  var stitle = 'Log In or Sign Up to join the discussion'
-  res.render('indexView', {
+
+  var locals = { 
     pageHeader: {
-      title: htitle
+      title: 'Election App 2016!'
     },
-    subtitle: stitle
+    subtitle: 'Log In or Sign Up to join the discussion'
+  }
+
+  res.render('indexView', locals, function (err, html) {
+
+    if (err) {
+      return next(err)
+
+    } else {
+      res.send(html)
+    }
   })
 }
-/*
-module.exports.getIndex = function(req, res, next){
-  var newCustomError;
-  var requestOptions, path;
-  path = '/api/index';
-
-  requestOptions = {
-    rejectUnauthorized: false,
-    url : apiOptions.server + path,
-    method : 'GET',
-    json : {}
-  };
-
-  request(requestOptions, function(err, response) {
-    //return next(createError(400, 'Bad Request.'));
-
-    if(res.app.locals.foober === true){
-      //res.app.locals.foober = false;
-      //return next(createError(400, 'Bad Request.'));
-    }
-
-    if(err){
-      return next(err);
-
-    }else if (response.statusCode === 200) {
-      var htitle = 'Election App 2016!';
-      var stitle = 'Log In or Sign Up to join the discussion';
-      res.render('indexView', {
-        pageHeader: {
-          title: htitle
-        },
-        subtitle: stitle
-      })
-
-    }else{
-      newCustomError = new Error('Bad Request');
-      newCustomError.status = 400;
-      return next(newCustomError);
-
-    }
-  });
-};
-*/
-
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++ */
 /* +++++++++++++++++++++++++++++++++++++++++++++++++ */
@@ -249,16 +224,24 @@ module.exports.getAddNewComment = function(req, res) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 module.exports.getLogin = function (req, res, next) {
+
   req.session.regenerate(function (err) {
+
     if (err) {
       return next(err)
 
     } else {
-      console.log('######### SERVER ########### getLogin ++++')
 
-      res.render('login', {
-        csrfToken: req.csrfToken()
+      res.render('login', { csrfToken: req.csrfToken() }, function (err, html) {
+
+        if (err) {
+          return next(err)
+
+        } else {
+          res.send(html)
+        }
       })
+
     }
   })
 }
@@ -267,21 +250,31 @@ module.exports.getLogin = function (req, res, next) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 module.exports.getSignup = function (req, res, next) {
+
   req.session.regenerate(function (err) {
+
     if (err) {
       return next(err)
 
     } else {
-      res.render('signup', {
-        csrfToken: req.csrfToken()
 
+      res.render('signup', { csrfToken: req.csrfToken() }, function (err, html) {
+
+        if (err) {
+          return next(err)
+
+        } else {
+          res.send(html)
+        }
       })
+
     }
   })
 }
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++ */
 /* +++++++++++++++++++++++++++++++++++++++++++++++++ */
+
 module.exports.getUserHome = function (req, res, next) {
 
   res.render('userHome', { err: req.session.renderableErr }, function (err, html) {
@@ -356,11 +349,21 @@ module.exports.getUserProfile = function (req, res, next) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++ */
 /* +++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-module.exports.getMembersOnly = function (req, res) {
-  res.render('membersonly', {
+module.exports.getMembersOnly = function (req, res, next) {
+
+  var locals = { 
     title: 'Members Only Page',
     pageHeader: {
       header: 'Hello Authorized Users!'
+    }
+  }
+
+  res.render('membersonly', locals, function (err, html) {
+    if (err) {
+      return next(err)
+
+    } else {
+      res.send(html)
     }
   })
 }
@@ -369,7 +372,7 @@ module.exports.getMembersOnly = function (req, res) {
 /* +++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 module.exports.getNotifyError = function (req, res, next) {
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> getNotifyError > notifyErrorMessageObject <<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> server > getNotifyError <<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
 
   res.render('notifyError', { err: req.session.renderableErr }, function (err, html) {
 
@@ -383,91 +386,117 @@ module.exports.getNotifyError = function (req, res, next) {
   })
 }
 
-/*
-module.exports.renderNotifyError = function (req, res) {
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> renderNotifyError <<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+/* +++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-  if (req.session.notifyErrorMessageObject) {
-    res.locals.notifyErrorMessageObject = req.session.notifyErrorMessageObject
-  }
-
-  var notifyMessage = 'A website error recently occurred, please try to Log In or Sign Up again. If this problem continues, please contact customer service.'
-  var notifyMessageType = 'danger'
-
-  res.render('notifyError', {
-    message: notifyMessage,
-    type: notifyMessageType
-  })
-
-}
-
-module.exports.getNotifyError = function (req, res, next) {
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> getNotifyError > notifyErrorMessageObject <<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
-
-  req.logout()
-  req.session.destroy(function (err) {
+module.exports.getLoginOrSignup = function (req, res, next) {
+  res.render('loginorsignup', function (err, html) {
     if (err) {
       return next(err)
     } else {
-      res.redirect('/rendernotifyerror')
+      res.send(html)
     }
   })
 }
-*/
 
-/* +++++++++++++++++++++++++++++++++++++++++++++++++ */
-/* +++++++++++++++++++++++++++++++++++++++++++++++++ */
+module.exports.getResouces = function (req, res, next) {
 
-module.exports.getLoginOrSignup = function (req, res) {
-  res.render('loginorsignup', {
-    /* +++++++= */
-  })
-}
-
-module.exports.getResouces = function (req, res) {
-  res.render('basicView', {
+  var locals = { 
     title: 'Resources',
     header: 'Resouces!',
     content: 'ThisGreatApp! is all about people sharing their favorite novelties across America.\n\nAut tenetur sit quam aliquid quia dolorum voluptate. Numquam itaque et hic reiciendis. Et eligendi quidem officia maiores. Molestiae ex sed vel architecto nostrum. Debitis culpa omnis perspiciatis vel eum. Vitae doloremque dolor enim aut minus.\n\nPossimus quaerat enim voluptatibus provident. Unde commodi ipsum voluptas ut velit. Explicabo voluptas at alias voluptas commodi. Illum et nihil ut nihil et. Voluptas iusto sed facere maiores.'
+    }
+
+  res.render('basicView', locals, function (err, html) {
+    if (err) {
+      return next(err)
+    } else {
+      res.send(html)
+    }
   })
 }
 
-module.exports.getDummyPage = function (req, res) {
-  res.render('basicView', {
+module.exports.getDummyPage = function (req, res, next) {
+
+  var locals = { 
     title: 'Dummy Test Page',
     header: 'Dummy Test Page!',
     content: 'Dummy Page content.\n\nThisGreatApp! is all about people sharing their favorite novelties across America.\n\nAut tenetur sit quam aliquid quia dolorum voluptate. Numquam itaque et hic reiciendis. Et eligendi quidem officia maiores. Molestiae ex sed vel architecto nostrum. Debitis culpa omnis perspiciatis vel eum. Vitae doloremque dolor enim aut minus.\n\nPossimus quaerat enim voluptatibus provident. Unde commodi ipsum voluptas ut velit. Explicabo voluptas at alias voluptas commodi. Illum et nihil ut nihil et. Voluptas iusto sed facere maiores.v'
+    }
+
+  res.render('basicView', locals, function (err, html) {
+    if (err) {
+      return next(err)
+    } else {
+      res.send(html)
+    }
   })
 }
 
-module.exports.getAbout = function (req, res) {
-  res.render('basicView', {
+module.exports.getAbout = function (req, res, next) {
+
+  var locals = { 
     title: 'About',
     header: 'About!',
     content: 'ThisGreatApp! is all about people sharing their favorite novelties across America.\n\nAut tenetur sit quam aliquid quia dolorum voluptate. Numquam itaque et hic reiciendis. Et eligendi quidem officia maiores. Molestiae ex sed vel architecto nostrum. Debitis culpa omnis perspiciatis vel eum. Vitae doloremque dolor enim aut minus.\n\nPossimus quaerat enim voluptatibus provident. Unde commodi ipsum voluptas ut velit. Explicabo voluptas at alias voluptas commodi. Illum et nihil ut nihil et. Voluptas iusto sed facere maiores.'
+    }
+
+  res.render('basicView', locals, function (err, html) {
+    if (err) {
+      return next(err)
+    } else {
+      res.send(html)
+    }
   })
 }
 
-module.exports.getContact = function (req, res) {
-  res.render('basicView', {
+module.exports.getContact = function (req, res, next) {
+
+  var locals = { 
     title: 'Contact',
     header: 'Contact!',
     content: 'ThisGreatApp! can be contacted by calling 1-800-555-1234.\n\nDolorem necessitatibus aliquam libero magni. Quod quaerat expedita at esse. Omnis tempora optio laborum laudantium culpa pariatur eveniet consequatur.'
+    }
+
+  res.render('basicView', locals, function (err, html) {
+    if (err) {
+      return next(err)
+    } else {
+      res.send(html)
+    }
   })
 }
 
-module.exports.getTeam = function (req, res) {
-  res.render('basicView', {
+module.exports.getTeam = function (req, res, next) {
+
+  var locals = { 
     title: 'Team',
     header: 'Meet the Team',
     content: 'The team behind ThisGreatApp! are a dedicated bunch who enjoy sharing favorite places and experiences.\n\nAt vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
+    }
+
+  res.render('basicView', locals, function (err, html) {
+    if (err) {
+      return next(err)
+    } else {
+      res.send(html)
+    }
   })
 }
 
-module.exports.getCustomerService = function (req, res) {
-  res.render('basicView', {
+module.exports.getCustomerService = function (req, res, next) {
+
+  var locals = { 
     title: 'Customer Service',
     header: 'ThisGreatApp\'s Customer Service',
     content: 'We at ThisGreatApp are dedicated to providing the highest level of customer service.\n\nAt vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
+    }
+
+  res.render('basicView', locals, function (err, html) {
+    if (err) {
+      return next(err)
+    } else {
+      res.send(html)
+    }
   })
 }
